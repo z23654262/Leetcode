35034a361ef9645bfc86da676cfe8db47996d613
z93478462 <1119716380@qq.com>
2021-08-05 15:38:24 +0800

    138、743、1893

---
+++ src/daily/Problem_743.java
@@ -0,0 +1,15 @@
+package daily;
+
+/**
+ * 743.网络延迟时间
+ * https://leetcode-cn.com/problems/network-delay-time/
+ * @author sfzhang
+ * @since 2021年8月2日
+ */
+public class Problem_743 {
+	public int networkDelayTime(int[][] times, int n, int k) {
+		int result = 0;
+		
+		return result;
+    }
+}
